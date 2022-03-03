c0628c0ff0ff524b1c26e8567dea8c3c296df217
SuperSerg58 <showtime5830@gmail.com>
2022-03-03 15:49:17 +0300

    'robots.txt'

--- App/urls.py
+++ App/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView, TemplateView
 from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")), ),
+    path('robots.txt', TemplateView.as_view(template_name='robot.txt', content_type='text/plain')),
     path('godmodeon/', admin.site.urls),
     path('todolist/', include('todolist.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
